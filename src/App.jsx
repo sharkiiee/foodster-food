a29407600651b8e66e790c813a816801a4960bab
priyu-31 <priyanshiagrawal3113@gmail.com>
2025-08-02 13:20:50 +0530

    01_recipe_showcase

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import Header from "./Components/Header";
-
+import Navbar from "./Components/Navbar";
+import { Data } from "./Components/Data";
+import { useState } from "react";
 export default function App() {
   return (
     <div className="box">
+      <Navbar />
       <Header />
-      Below the header
+
+      <Grid data={Data} />
+    </div>
+  );
+}
+
+function Grid({ data }) {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleSelection(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+
+  return (
+    <div className="grid-container">
+      {data.map((item) => (
+        <div
+          key={item.id}
+          className="boxes"
+          onClick={() => handleSelection(item.id)}>
+          <img src={item.img} alt={item.name} />
+          <h1>{item.name}</h1>
+          <p>{item.id == selectedId ? item.recipe : item.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
